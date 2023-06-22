478b38bc52009412fe8466e42a5ff1b93b7e0891
Jeff Mesnil <jmesnil@redhat.com>
2023-06-22 10:27:50 +0200

    updated instructions
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

---
+++ push.sh
@@ -0,0 +1,5 @@
+oc registry login
+export OC_REGISTRY=$(oc registry info)
+OC_IMAGESTREAM=$OC_REGISTRY/$(oc project -q)/eap8-ga-app
+docker tag eap8-ga-app $OC_IMAGESTREAM
+docker push $OC_IMAGESTREAM
