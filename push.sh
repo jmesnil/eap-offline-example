730ff6b783e4d6bf68baced246ba4d0a53423101
Jeff Mesnil <jmesnil@redhat.com>
2023-06-22 10:35:34 +0200

    update the image lookup-policy
    Signed-off-by: Jeff Mesnil <jmesnil@redhat.com>

--- push.sh
+++ push.sh
@@ -3,3 +3,4 @@ export OC_REGISTRY=$(oc registry info)
 OC_IMAGESTREAM=$OC_REGISTRY/$(oc project -q)/eap8-ga-app
 docker tag eap8-ga-app $OC_IMAGESTREAM
 docker push $OC_IMAGESTREAM
+oc set image-lookup eap8-ga-app
